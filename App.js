408460e8a9bdcbc7b60f5d2aee085f4865ea236f
Reza97ab <reza97.ab@gmail.com>
2023-01-14 11:18:31 +0330

    Initialize project using Create React App

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+};
+
+export default App;
